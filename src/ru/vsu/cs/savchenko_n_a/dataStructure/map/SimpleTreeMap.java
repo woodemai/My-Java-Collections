c08fc58d968cc7d3ff55080296eee5cb3390696e
woodemai <n.savchenkoo73@gmail.com>
2023-06-27 23:02:09 +0300

    inputs

--- src/ru/vsu/cs/savchenko_n_a/dataStructure/map/SimpleTreeMap.java
+++ src/ru/vsu/cs/savchenko_n_a/dataStructure/map/SimpleTreeMap.java
@@ -1,19 +1,67 @@
 package ru.vsu.cs.savchenko_n_a.dataStructure.map;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SimpleTreeMap<K extends Comparable<K>, V> {
-    private Node root;
+    private Node<K, V> root;
+    private final Comparator<? super K> comparator;
+    public SimpleTreeMap() {
+        comparator = null;
+    }
 
-    private class Node {
+    public SimpleTreeMap(Comparator<? super K> comparator) {
+        this.comparator = comparator;
+    }
+
+    private static class Node<K, V> {
         K key;
         V value;
-        Node left, right;
+
+        public K getKey() {
+            return key;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public Node<K, V> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<K, V> left) {
+            this.left = left;
+        }
+
+        public Node<K, V> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<K, V> right) {
+            this.right = right;
+        }
+
+        Node<K, V> left, right;
 
         Node(K key, V value) {
             this.key = key;
             this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
         }
     }
 
@@ -21,11 +69,10 @@ public class SimpleTreeMap<K extends Comparable<K>, V> {
         root = put(root, key, value);
     }
 
-    private Node put(Node node, K key, V value) {
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
         if (node == null) {
-            return new Node(key, value);
+            return new Node<>(key, value);
         }
-
         if (compare(key, node.key) < 0) {
             node.left = put(node.left, key, value);
         } else if (compare(key, node.key) > 0) {
@@ -38,11 +85,11 @@ public class SimpleTreeMap<K extends Comparable<K>, V> {
     }
 
     public V get(K key) {
-        Node node = get(root, key);
-        return node == null ? null : node.value;
+        Node<K,V> node = get(root, key);
+        return node == null ? null : node.getValue();
     }
 
-    private Node get(Node node, K key) {
+    private Node<K,V> get(Node<K,V> node, K key) {
         if (node == null) {
             return null;
         }
@@ -55,6 +102,7 @@ public class SimpleTreeMap<K extends Comparable<K>, V> {
             return node;
         }
     }
+
     public boolean containsKey(K key) {
         return get(root, key) != null;
     }
@@ -62,19 +110,21 @@ public class SimpleTreeMap<K extends Comparable<K>, V> {
     private int compare(K key1, K key2) {
         return key1.compareTo(key2);
     }
+
     public Set<K> keySet() {
-        return keySet(root, new HashSet<>());
+        return keySet(root, new TreeSet<>());
     }
 
-    private Set<K> keySet(Node node, Set<K> keySet) {
+    private Set<K> keySet(Node<K,V> node, Set<K> keySet) {
         if (node == null) {
             return keySet;
         }
 
-        keySet.add(node.key);
         keySet(node.left, keySet);
+        keySet.add(node.key);
         keySet(node.right, keySet);
 
         return keySet;
     }
+
 }
